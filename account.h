9f6c0bea5eb2309c273fac8278464ff99c6ff3d6
“sumikkys” <me@sumi.link>
2024-07-21 20:25:08 +0800

    完善类定义

--- account.h
+++ account.h
@@ -1,10 +1,32 @@
 #ifndef ACCOUNT_H
 #define ACCOUNT_H
 
+#include<QString>
+
 class Account
 {
+private:
+    QString cardNumber; // 卡号
+    QString password; // 密码
+    unsigned int balance; // 余额 数值10000表示为100.00元
+
 public:
-    Account();
+    // 构造函数
+    Account(const QString& cardNumber, const QString& password, const unsigned int balance);
+
+    // 获取余额
+    unsigned int getBalance();
+
+    // 取款
+    bool withdraw(unsigned int amount);
+
+    // 存款
+    bool deposit(unsigned int amount);
+
+    // 修改密码
+    bool changePassword(const QString& oldPassword, const QString& newPassword);
+
+
 };
 
 #endif // ACCOUNT_H
--- atmsystem.h
+++ atmsystem.h
@@ -1,10 +1,30 @@
 #ifndef ATMSYSTEM_H
 #define ATMSYSTEM_H
+#include <QString>
+#include<account.h>
 
 class AtmSystem
 {
+private:
+    Account account;
 public:
+    // 构造函数
     AtmSystem();
+
+    // 登陆 登陆成功时返回true给LoginWidget::handleLogin函数 并创建account
+    bool login(const QString& cardNumber, const QString& password);
+
+    // 查询余额
+    unsigned int checkBalance();
+
+    // 取款
+    bool withdraw(unsigned int amount);
+
+    // 存款
+    bool deposit(unsigned int amount);
+
+    // 修改密码
+    bool changePassword(const QString& oldPassword, const QString& newPassword);
 };
 
 #endif // ATMSYSTEM_H
